42db3ebb031fd970e595d968077d216421b1b986
ckomalram <glaw14@gmail.com>
2023-03-18 16:21:40 -0500

    ++ Reemplazar servicios de angular por falsos

--- src/app/avanzado/router-medico/router-medico.component.spec.ts
+++ src/app/avanzado/router-medico/router-medico.component.spec.ts
@@ -1,6 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
 import { RouterMedicoComponent } from './router-medico.component';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable, EMPTY } from 'rxjs';
+
+class Fakerouter {
+  navigate(params: any) {
+    console.log(params);
+  }
+}
+
+class Fakeactivatedroute {
+  params: Observable<any> = EMPTY;
+}
 
 describe('RouterMedicoComponent', () => {
   let component: RouterMedicoComponent;
@@ -8,16 +20,27 @@ describe('RouterMedicoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RouterMedicoComponent ]
-    })
-    .compileComponents();
+      declarations: [RouterMedicoComponent],
+      providers: [{ provide: Router, useClass: Fakerouter },{ provide: ActivatedRoute, useClass: Fakeactivatedroute } ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RouterMedicoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('Redireccionar a medico cuando se guarde.', inject([Router], (router: Router)  => {
+    // const router = TestBed.get(Router);
+    const spy = spyOn(router, 'navigate');
+
+    component.guardarMedico();
+
+    // console.log(spy);
+
+    expect(spy).toHaveBeenCalledWith(['medico', '123'])
+  }));
+
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
