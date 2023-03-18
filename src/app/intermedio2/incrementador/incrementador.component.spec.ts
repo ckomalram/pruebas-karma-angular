4c2b71955101bd6a4ebecadecfcb15d801da1878
ckomalram <glaw14@gmail.com>
2023-03-18 14:51:16 -0500

    ++ prueba con elementos de HTML

--- src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -1,28 +1,34 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
-
+import { By } from '@angular/platform-browser';
 
 describe('Incremendator Component', () => {
+  let component: IncrementadorComponent;
+  let fixture: ComponentFixture<IncrementadorComponent>;
 
-    let component: IncrementadorComponent;
-    let fixture: ComponentFixture<IncrementadorComponent>;
-
-    beforeEach( () => {
-        TestBed.configureTestingModule({
-            declarations: [ IncrementadorComponent ],
-            imports: [ FormsModule ]
-        });
-
-        fixture = TestBed.createComponent(IncrementadorComponent);
-        component = fixture.componentInstance;
-
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [IncrementadorComponent],
+      imports: [FormsModule],
     });
 
-    it('IncrementadorComponent', () => {
+    fixture = TestBed.createComponent(IncrementadorComponent);
+    component = fixture.componentInstance;
+  });
 
-      expect(true).toBeTruthy();
+  it('HTML: Debe mostrar Leyenda', () => {
+    component.leyenda = 'Carlyle Komalram';
 
-    });
+    // Para disparar detección de cambios de angular
+    fixture.detectChanges();
 
+    //con query se extrae el primero que se encuentre en el html
+    //fixture es el elemento compilado de angular
+    const element: HTMLElement = fixture.debugElement.query(
+      By.css('h3')
+    ).nativeElement;
+
+    expect(element.innerHTML).toContain('Carlyle Komalram');
+  });
 });
