a44f9946addc28320439de9377e7357894a29b7d
ckomalram <glaw14@gmail.com>
2023-03-18 10:38:33 -0500

    ++ arreglos

---
+++ src/app/basicas/arreglos/arreglos.spec.ts
@@ -0,0 +1,22 @@
+import { getRobots } from './arreglos';
+
+
+describe('Prueba de arreglos', () => {
+  it('Retornar al menos 3 valores', () => {
+    const rta = getRobots();
+
+    expect(rta.length).toBeGreaterThanOrEqual(3);
+  });
+
+  it('Debe existir Optimus y Mazinger', () => {
+
+    const rta = getRobots();
+
+    expect(rta).toContain('Megaman');
+    expect(rta).toContain('Optimus');
+  });
+
+
+
+
+});
